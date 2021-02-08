f3f13c7fba08b15f6caa1a89d6a89fcbdc3efac9
lumiere13 <60005583+lumiere13@users.noreply.github.com>
2021-02-08 16:36:53 +0700

    Week 1 dan 2

---
+++ Week01_28280/app/src/main/java/id/ac/umn/week01_28280/MainActivity.java
@@ -0,0 +1,17 @@
+package id.ac.umn.week01_28280;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+//tambahan
+import androidx.recyclerview.widget.RecyclerView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
